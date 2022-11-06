89024a608529e24abc4accbe629e8a738ca2516d
Eric Marcon <eric.marcon@agroparistech.fr>
2022-11-06 13:15:15 +0100

    Identifiant G4

--- gtag.js
+++ gtag.js
@@ -1,5 +1,5 @@
 <!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-163419632-1"></script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-C1B3PLBCM6"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
   function gtag(){dataLayer.push(arguments);}
